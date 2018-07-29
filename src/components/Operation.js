17dc6a2bf0c39c00bede1fa08ac60f7ff563d921
Nicollas <n.matheuscarvalho@gmail.com>
2018-07-29 00:27:01 -0300

    picker working

--- src/components/Operation.js
+++ src/components/Operation.js
@@ -6,9 +6,16 @@ import {
 } from 'react-native';
 
 export default class Operation extends Component {
+    constructor(props){
+        super(props)
+        this.state ={
+            operation: ''
+        }
+    }
     render(){
         return(
-            <Picker itemStyle={generalPicker}>
+            <Picker itemStyle={generalPicker} selectedValue={this.state.operation}
+                    onValueChange={value => this.setState({operation: value})}>
                 <Picker.item label="Soma" value="soma"/>
                 <Picker.item label="Subtracao" value="subtracao"/>
                 <Picker.item label="Multiplicação" value="multiplicacao"/>
